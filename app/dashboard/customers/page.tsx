bf7f7401206f383a1c0ec824bfd248864a7e826e
raghuwar-jangir <raghujangid222@gmail.com>
2024-01-25 19:41:38 +0530

    implement authentication to the app

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Customers() {
+  
   return (
     <>
       <div>Customers Page</div>
--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,9 +1,18 @@
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 export default async function Page({ params }: { params: { id: string } }) {
     const {id} = params;
     const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+    if(!invoice) {
+      notFound();
+    }
   return (
     <main>
       <Breadcrumbs
